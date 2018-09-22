2e7ea513a83a06f606d409f74956b905f779e509
Josiah Holmes <josiahkholmes@gmail.com>
2018-09-22 10:04:29 -0400

    First commit

---
+++ python/exes_and_ohs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+"""
+Check to see if a string has the same 
+amount of 'x's and 'o's. The method must 
+return a boolean and be case insensitive. 
+The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+"""
+
+
+def xo(s):
+    s = s.lower()
+    print(s.count('o') == s.count('x'))
+
+
+# Tests
+xo('ooxx') # True
+xo('xooxx') # False
+xo('ooxXm') # True
+xo('zpzpzpp') # True
+xo('zzoo') # False
