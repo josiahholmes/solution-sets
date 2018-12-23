02e6aa5bc21960da8d61963d38ee9c57cc48c0b5
Josiah Holmes <josiahkholmes@gmail.com>
2018-12-23 01:05:54 -0500

    pos neg challenge - initial commit

--- python/coding_bat/warmup-1/pos_neg.py
+++ python/coding_bat/warmup-1/pos_neg.py
@@ -5,8 +5,12 @@ def pos_neg(a, b, negative):
     if the parameter "negative" is True, then 
     return True only if both are negative.
     '''
-    return ((a and b) or negative) if not negative else True
+    if negative:
+        return (a < 0 and b < 0)
+    return ((a > 0 and b < 0) or (a < 0 and b > 0))
 
 print(pos_neg(1, -1, False)) # True
 print(pos_neg(-1, 1, False)) # True
 print(pos_neg(-4, -5, True)) # True
+
+
